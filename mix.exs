ad0d8610c38acc6b09e7acf55d78a8e21744e244
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-08-13 23:40:26 +0200

    decrease min elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Renewex.MixProject do
     [
       app: :renewex,
       version: "0.3.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
